96042282766a809aa90e38e8db06f0dc4df58611
Nader <na511dr@hotmail.com>
2020-03-26 22:43:24 +0300

    parrot_trouble

---
+++ parrot_trouble.py
@@ -0,0 +1,9 @@
+def parrot_trouble(talking, hour):
+    return (talking and (hour < 7 or hour > 20))
+
+
+print(parrot_trouble(True, 6))
+print(parrot_trouble(True, 7))
+print(parrot_trouble(False, 6))
+
+print(parrot_trouble(True, 21))
