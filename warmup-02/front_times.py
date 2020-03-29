05dbc6600ed8f46b752f1db08a87cb4ee81f8e83
Nader <na511dr@hotmail.com>
2020-03-29 22:38:07 +0300

    front_times example

---
+++ warmup-02/front_times.py
@@ -0,0 +1,8 @@
+def front_times(str, n):
+    return str[0:3]*n
+
+
+print(front_times('Chocolate', 2))
+print(front_times('Chocolate', 3))
+print(front_times('Abc', 3))
+print(front_times("na", 3))
