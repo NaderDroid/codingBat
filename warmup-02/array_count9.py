2b022760458134fa538f49cf4151f8d36a18125f
Nader <na511dr@hotmail.com>
2020-03-31 21:57:19 +0300

    array_count9 example

---
+++ warmup-02/array_count9.py
@@ -0,0 +1,12 @@
+def array_count9(nums):
+    count = 0
+    for i in nums:
+        if i == 9:
+            count += 1
+    return count
+
+
+print(array_count9([1, 2, 9]))
+print(array_count9([1, 9, 9]))
+print(array_count9([1, 9, 9, 3, 9]))
+print(array_count9([1, 3, 0, 4, 9]))
