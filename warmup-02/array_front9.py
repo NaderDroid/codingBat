73f55a2f17acd32afff9df9d7e10ddd322b457dd
Nader <na511dr@hotmail.com>
2020-04-02 00:25:57 +0300

    array_front9 example completed

--- warmup-02/array_front9.py
+++ warmup-02/array_front9.py
@@ -1,17 +1,22 @@
 def array_front9(nums):
+    print(len(nums))
     val = 0
     while val < len(nums):
-        if nums[val] == 9 and val < 4:
+        if nums[val] == 9 and val <= 3:
             return True
-        elif val > 3:
+        elif val > 3 or val == (len(nums)-1):
             return False
-        val += 1
-        # else:
-        #     print(n)
-        #     return False
+        else:
+            val += 1
+    return False
+    # else:
+    #     print(n)
+    #     return False
 
 
-print(array_front9([1, 2, 3, 4, 9]), "check this one here too")
+print(array_front9([]))
+print(array_front9([1, 2, 3]))
+print(array_front9([1, 2, 3, 4, 9]), "here too")
 print(array_front9([1, 9, 9]), "check this one")
 print(array_front9([1, 2, 9, 3, 4]))
 print(array_front9([1, 2, 3, 4, 9]))
